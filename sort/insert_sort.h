de1c84fc93d49bf5d8f0f6b6b408d1749c643473
Yang Yansheng <yangyansheng@sensenets.com>
2016-05-10 20:00:25 +0800

    Add the hash module

--- sort/insert_sort.h
+++ sort/insert_sort.h
@@ -17,7 +17,7 @@ void InsertSort(std::vector<T> &array)  {
 		int j;
 		// insert temp into the sorted part.
 		for (j = i - 1; j >= 0 && array[j] > temp; j--) {
-				array[j + 1] = array[j];
+			array[j + 1] = array[j];
 		}
 		// now array[j] <= temp, so insert temp after j.
 		array[j + 1] = temp;
